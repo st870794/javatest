8ddddce12f5a91f576b44d7d430e9e1e76c80815
Jiaren <st870794@gmail.com>
2020-09-08 11:30:41 +0800

    switch function

---
+++ Test04.java
@@ -0,0 +1,34 @@
+package ch03;
+
+import java.util.Scanner;
+
+public class Test04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int day = scanner.nextInt();
+			switch(day)
+			{
+			case 1:
+				System.out.println("wear new clothes");
+				break;
+			case 2:
+				System.out.println("hungry!");
+				break;
+			case 3:
+				System.out.println("climb mountains");
+				break;
+			case 4:
+				System.out.println("watch TV");
+				break;
+			case 5:
+				System.out.println("dance!");
+				break;
+			case 6:
+				System.out.println("go to Douliou");
+				break;
+		}		
+	}
+}
+
